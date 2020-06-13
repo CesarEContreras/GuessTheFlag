63cbc456b85a7f4f4fb06b3428a1c875cc1191a1
Cesar Contreras <cesar.contrerasm@outlook.com>
2020-06-13 18:57:43 -0400

    Playing around with VStack and HStack

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -10,7 +10,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack(spacing: 20) {
+            HStack(spacing: 10) {
+                Text("Hello, World1!")
+                Text("Hello, World2!")
+                Text("Hello, World3!")
+            }
+            
+            HStack(spacing: 10) {
+                Text("Hello, World4!")
+                Text("Hello, World5!")
+                Text("Hello, World6!")
+            }
+            
+            HStack(spacing: 10) {
+                Text("Hello, World7!")
+                Text("Hello, World8!")
+                Text("Hello, World9!")
+            }
+        }
     }
 }
 
