3b3aeaee013734d7c8dd393251176aa8806f936f
Cesar Contreras <cesar.contrerasm@outlook.com>
2020-06-17 21:50:20 -0400

    Using colors and frames

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -10,24 +10,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing: 20) {
-            HStack(spacing: 10) {
-                Text("Hello, World1!")
-                Text("Hello, World2!")
-                Text("Hello, World3!")
-            }
-            
-            HStack(spacing: 10) {
-                Text("Hello, World4!")
-                Text("Hello, World5!")
-                Text("Hello, World6!")
-            }
-            
-            HStack(spacing: 10) {
-                Text("Hello, World7!")
-                Text("Hello, World8!")
-                Text("Hello, World9!")
-            }
+        ZStack {
+            Color.blue.edgesIgnoringSafeArea(.all)
+            Text("My content")
         }
     }
 }
