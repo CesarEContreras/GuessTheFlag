3ff4bfb75c66fa2be87861e7800401bb07a8b328
Cesar Contreras <cesar.contrerasm@outlook.com>
2020-06-17 22:07:47 -0400

    Showing alert messages

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,48 +9,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
+    
     var body: some View {
-        VStack {
-            Button("Tap me!") {
-                print("Button was tapped")
-            }
-            
-            Button(action: {
-                print("Button was tapped")
-            }) {
-                Text("Tap me!")
-            }
-            
-            Button(action: {
-                print("Edit button was tapped")
-            }) {
-                Image(systemName: "pencil")
-            }
-            
-            Button(action: {
-                print("Edit button was tapped")
-            }) {
-                HStack(spacing: 10) {
-                    Image(systemName: "pencil")
-                    Text("Edit")
-                }
-            }
-            
-            Button(action: {
-                print("Edit button was tapped")
-            }) {
-                HStack(spacing: 10) {
-                    Image(systemName: "pencil")
-                        .renderingMode(.original)
-                    Text("Edit")
-                }
-            }
+        Button("Show Alert") {
+            self.showingAlert = true
+        }
+        .alert(isPresented: $showingAlert) {
+            Alert(title: Text("Hello SwiftUI"),
+                  message: Text("This is some detail message"),
+                  dismissButton: .default(Text("OK")))
         }
     }
 }
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
 }
