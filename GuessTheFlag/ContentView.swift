b8d07b5aae36f48e8bf3fd98eb3c0ab481e63af4
Cesar Contreras <cesar.contrerasm@outlook.com>
2020-06-17 21:54:57 -0400

    Using Gradients

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -10,9 +10,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Color.blue.edgesIgnoringSafeArea(.all)
-            Text("My content")
+        VStack {
+            LinearGradient(gradient: Gradient(colors: [.white, .black]), startPoint: .top, endPoint: .bottom)
+            
+            RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 20, endRadius: 200)
+            
+            AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center)
         }
     }
 }
